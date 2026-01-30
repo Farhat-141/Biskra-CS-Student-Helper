5192ec0dbf0afe836bab2233cc4cf4c5889d62c5
Ford <farhatagraine@gmail.com>
2026-01-30 22:22:18 +0100

    second test for oflline mod

--- sw.js
+++ sw.js
@@ -1,30 +1,46 @@
-const CACHE = "biskra-cs-v1";
+const CACHE = "biskra-cs-v2";
+
 const ASSETS = [
-  ".",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icon.png",
-  "./toggle2.png"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icon.png",
+  "/toggle2.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => (k !== CACHE ? caches.delete(k) : null))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  if (e.request.mode === "navigate") {
-    e.respondWith(caches.match("./index.html"));
+// Fetch
+self.addEventListener("fetch", event => {
+  // IMPORTANT: handle navigations
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      caches.match("/index.html")
+    );
     return;
   }
 
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
